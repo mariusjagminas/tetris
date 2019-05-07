bb1d27664082353d391e7397a6436bcf48045999
mariusjagminas <mariusjagminas.it@gmail.com>
2019-05-07 15:59:27 +0200

    Updates

---
+++ precache-manifest.6fc733f6bed444a645ba1207a3b3568f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa45b599783248103ee261f989a6ebcc",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "25027dcf1561b891d665",
+    "url": "/tetris/static/css/main.be909c2b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tetris/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "25027dcf1561b891d665",
+    "url": "/tetris/static/js/main.9d4edd0e.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  }
+]);
