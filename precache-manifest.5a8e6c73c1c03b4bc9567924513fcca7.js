8cbbc8ddd21977cab680e58053d5669f20ca2638
mariusjagminas <mariusjagminas.it@gmail.com>
2019-05-24 15:24:39 +0200

    Updates

---
+++ precache-manifest.5a8e6c73c1c03b4bc9567924513fcca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e553da0ddfcaab363995a1c56d244e24",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "99acc1440b6ac080051f",
+    "url": "/tetris/static/css/main.7482da3a.chunk.css"
+  },
+  {
+    "revision": "33f14f40ecd1b24477ad",
+    "url": "/tetris/static/js/2.71e5ccca.chunk.js"
+  },
+  {
+    "revision": "99acc1440b6ac080051f",
+    "url": "/tetris/static/js/main.9d824e09.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  },
+  {
+    "revision": "8aec498c4a46d2bab626da1f82d7f124",
+    "url": "/tetris/static/media/digital-7.8aec498c.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/tetris/precache-manifest.7ac7f36f289432408c2c6b8555f94a0c.js"
+  "/tetris/precache-manifest.5a8e6c73c1c03b4bc9567924513fcca7.js"
 );
 
 self.addEventListener('message', (event) => {
